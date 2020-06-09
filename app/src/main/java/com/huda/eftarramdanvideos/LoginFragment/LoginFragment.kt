bc383431008fc7931012e5a061cb02624642b3da
Huda Mohamed <huda.fci@gmail.com>
2020-06-09 08:50:45 -0700

    change ui and fix issues

--- app/src/main/java/com/huda/eftarramdanvideos/LoginFragment/LoginFragment.kt
+++ app/src/main/java/com/huda/eftarramdanvideos/LoginFragment/LoginFragment.kt
@@ -13,7 +13,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
